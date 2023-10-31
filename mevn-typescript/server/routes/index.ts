a718d977ec760e91acd97fa3e8577e8b79b0fb36
patxi <patxiarana05@gmail.com>
2023-10-31 16:13:50 -0300

    connecting with database

--- mevn-typescript/server/routes/index.ts
+++ mevn-typescript/server/routes/index.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
+import Task from "../models/Task" ; 
 
 const router = Router() ; 
 
 
-router.get('/tasks' , (req,res) => {
-    res.send("getting tasks") ; 
+router.get('/tasks' , async (req,res) => {
+    try {
+  const tasks =   await Task.find() ; 
+  console.log(tasks)
+    res.send(tasks) ;
+    }catch(error) {
+        console.log(error)
+    } 
 
 })
 
 
 router.post('/tasks' , (req,res) => {
+  const {title, description} = req.body 
+
+ const newTask =  new Task({title,description}) ; 
+ console.log(newTask)
     res.send("creating task") ; 
 
 })
